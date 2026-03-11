85c2ca31f4e245a7fbdc7b26e7a5bf7c7659694a
KasraMg <kasragithub7@gmail.com>
2026-03-11 14:55:22 +0330

    feat(auth): update controller and add middlware

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,27 +4,17 @@ const bcrypt = require("bcryptjs");
 
 exports.getMe = async (req, res) => {
   try {
-    const authHeader = req.headers["authorization"];
-
-    if (!authHeader) {
-      return res.status(401).json({ message: "توکنی یافت نشد" });
-    }
-
-    const token = authHeader.startsWith("Bearer ")
-      ? authHeader.split(" ")[1]
-      : authHeader;
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); 
-
-    const user = await User.findById(decoded.id).select("-password");
+    const user = await User.findById(req.user.id).select("-password");
 
     if (!user) {
       return res.status(404).json({ message: "کاربری یافت نشد" });
-    }
-
-    res.json(user);
+    } 
+    res.json({
+      ...user.toObject(),
+      impersonatedBy: req.user.impersonatedBy || null,
+    });
   } catch (error) {
-    return res.status(401).json({ message: "توکن نامعتبر است" });
+    res.status(500).json({ message: "خطای سرور" });
   }
 };
 
@@ -89,4 +79,31 @@ exports.register = async (req, res) => {
       return res.status(400).json({ message: messages.join(" ") });
     }
   }
+};
+
+exports.impersonateUser = async (req, res) => {
+  const targetUser = await User.findById(req.params.userId);
+
+  if (!targetUser) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const token = jwt.sign(
+    {
+      id: targetUser._id,
+      role: targetUser.role,
+      impersonatedBy: req.user.id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "15m" },
+  );
+
+  res.json({
+    token,
+    impersonatedUser: {
+      id: targetUser._id,
+      name: targetUser.name,
+      phone: targetUser.phone,
+    },
+  });
 };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,18 +3,19 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
-    password: {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: Number, required: true, unique: true },
+    password: { type: String, required: true },
+
+    role: {
       type: String,
-      required: [true, "لطفاً رمز عبور را وارد کنید"],
-      minlength: [6, "رمز عبور باید حداقل 6 کاراکتر باشد"],
+      enum: ["user", "admin"],
+      default: "user",
     },
-    name: { type: String, required: [true, "نام الزامی است"] },
-    email: { type: String, required: [true, "ایمیل الزامی است"] },
-    phone: { type: Number, required: [true, "تلفن الزامی است"], unique: true },
   },
   { timestamps: true },
 );
-
 userSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
   const salt = await bcrypt.genSalt(10);
