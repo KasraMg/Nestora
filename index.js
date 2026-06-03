f4fb79e35db17d73badafaf0977bb9074855aaa9
KasraMg <kasragithub7@gmail.com>
2026-06-03 19:53:15 +0330

    feat: add cors package and allow local frontend request

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./src/config/db");
 
 const app = express();
@@ -11,13 +12,20 @@ const swaggerFile = require("./swagger-output.json");
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
+ 
 app.use("/api/auth", require("./src/routes/auth.routes"));
 app.use("/api", require("./src/routes/products.routes"));
 app.use("/api", require("./src/routes/banner.routes"));
