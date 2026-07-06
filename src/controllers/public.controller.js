bade80f297b515cbe682b8ca6e43c5cecba3c8b7
KasraMg <kasragithub7@gmail.com>
2026-07-06 18:02:57 +0330

    refactor(public): move business logic to service layer

--- src/controllers/public.controller.js
+++ src/controllers/public.controller.js
@@ -1,52 +1,21 @@
-const Category = require("../models/category.model");
-const Products = require("../models/products.model");
-const Province = require("../models/province.model");
-const City = require("../models/city.model");
+const publicService = require("../services/public.service");
 
 exports.getShopFilters = async (req, res, next) => {
   try {
-    const categories = await Category.find();
-    const colors = await Products.distinct("colors.name");
+    const data = await publicService.getShopFilters();
 
-    const priceRange = await Products.aggregate([
-      {
-        $group: {
-          _id: null,
-          minPrice: { $min: "$price" },
-          maxPrice: { $max: "$price" },
-        },
-      },
-    ]);
-
-    const minPrice = priceRange.length ? priceRange[0].minPrice : 0;
-    const maxPrice = priceRange.length ? priceRange[0].maxPrice : 0;
-
-    return res.status(200).json({
-      categories,
-      colors,
-      minPrice,
-      maxPrice,
-    });
+    res.status(200).json(data);
   } catch (error) {
     next(error);
   }
 };
 
-let cache = null;
+exports.getLocations = async (req, res, next) => {
+  try {
+    const data = await publicService.getLocations();
 
-exports.getLocations = async (req, res) => {
-  if (cache) {
-    return res.json(cache);
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
   }
-  const [provinces, cities] = await Promise.all([
-    Province.find({}, "provinceId provinceName").lean(),
-    City.find({}, "cityId cityName provinceId").lean(),
-  ]);
-
-  cache = { provinces, cities };
-
-  res.json({
-    provinces,
-    cities,
-  });
 };
