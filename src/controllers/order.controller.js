91e480c3d179c5738874522abaa5bee0fb14bf17
KasraMg <kasragithub7@gmail.com>
2026-07-06 18:02:43 +0330

    refactor(order): move business logic to service layer

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,57 +1,24 @@
-const Order = require("../models/order.model");
-const AppError = require("../utils/AppError");
+const orderService = require("../services/order.service");
 
 exports.getOrder = async (req, res, next) => {
-  const { trackingCode } = req.params;
   try {
-    if (!trackingCode) {
-      return next(new AppError("شناسه سفارش ارسال نشده است", 400));
-    }
-    const order = await Order.findOne({ trackingCode }).populate(
-      "products.product",
-    );
-    if (order) {
-      res.status(200).json(order);
-    } else {
-      return next(new AppError("سفارشی با این شناسه یافت نشد", 404));
-    }
+    const order = await orderService.getOrder(req.params.trackingCode);
+
+    res.status(200).json(order);
   } catch (error) {
     next(error);
   }
 };
+
 exports.createOrder = async (req, res, next) => {
-  const user = await req.user.populate("cart.product");
-  const { information } = req.body;
   try {
-    if (user.cart.length == 0) {
-      return next(new AppError("سبد خرید کاربر خالی است", 400));
-    }
-    const prices = user.cart.map((item) => item.quantity * item.product.price);
-    const totalPrice = prices?.reduce((a, b) => a + b, 0);
-
-    const trackingCode = crypto.randomUUID().slice(0, 5);
-    const order = new Order({
-      totalPrice,
-      products: user.cart.map((item) => {
-        return {
-          product: item.product,
-          color: item.color,
-          quantity: item.quantity,
-          sellPrice: item.product.price,
-        };
-      }),
-      user: user._id,
-      information,
-      status: "successfull",
-      trackingCode,
-    });
-
-    await order.save();
-    user.cart = [];
-    await user.save();
+    const { trackingCode } = await orderService.createOrder(
+      req.user,
+      req.body.information,
+    );
 
     res.status(201).json({
-      message: `سفارش شما با موفقیت ثبت شد`,
+      message: "سفارش شما با موفقیت ثبت شد",
       trackingCode,
     });
   } catch (error) {
---
+++ src/services/order.service.js
@@ -0,0 +1,59 @@
+const crypto = require("crypto");
+
+const Order = require("../models/order.model");
+const AppError = require("../utils/AppError");
+
+exports.getOrder = async (trackingCode) => {
+  if (!trackingCode) {
+    throw new AppError("شناسه سفارش ارسال نشده است", 400);
+  }
+
+  const order = await Order.findOne({ trackingCode }).populate(
+    "products.product",
+  );
+
+  if (!order) {
+    throw new AppError("سفارشی با این شناسه یافت نشد", 404);
+  }
+
+  return order;
+};
+
+exports.createOrder = async (user, information) => {
+  await user.populate("cart.product");
+
+  if (user.cart.length === 0) {
+    throw new AppError("سبد خرید کاربر خالی است", 400);
+  }
+
+  const totalPrice = user.cart.reduce(
+    (sum, item) => sum + item.quantity * item.product.price,
+    0,
+  );
+
+  const trackingCode = crypto.randomUUID().slice(0, 5);
+
+  const order = new Order({
+    totalPrice,
+    products: user.cart.map((item) => ({
+      product: item.product,
+      color: item.color,
+      quantity: item.quantity,
+      sellPrice: item.product.price,
+    })),
+    user: user._id,
+    information,
+    status: "successfull",
+    trackingCode,
+  });
+
+  await order.save();
+
+  user.cart = [];
+  await user.save();
+
+  return {
+    trackingCode,
+    order,
+  };
+};
